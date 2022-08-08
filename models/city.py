f60c18dc98fb1ae2c2ba97cc11f401dbc21772b0
MatiasMtz <4442@holbertonschool.com>
2022-08-08 15:28:45 +0000

    classes fixes dbstorage

--- models/city.py
+++ models/city.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from tkinter import CASCADE
+from turtle import back
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, ForeignKey, String
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all, delete-orphan", \
+                          backref="cities")
