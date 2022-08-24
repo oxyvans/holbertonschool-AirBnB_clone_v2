49256c4a3ff5239bf5e0fc74204e48fed219a381
oxyvans <benja.1999.g@hotmail.com>
2022-08-24 13:18:15 -0700

    add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Write a Fabric script """
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+def do_pack():
+    """ tgz """
+    try:
+        time = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        res = "versions/web_static_{}.tgz".format(time)
+        local("tar -cvzf {} web_static".format(res))
+        return res
+    except:
+        return None
