7359c78ce43f4bc5cf11ea5c82bf4587f9c93cd4
oxyvans <benja.1999.g@hotmail.com>
2022-08-25 09:42:00 -0700

    add task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" task 2 """
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['107.23.23.248', '107.21.59.242']
+
+
+def do_deploy(archive_path):
+    """ distribute files """
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except():
+        return False
