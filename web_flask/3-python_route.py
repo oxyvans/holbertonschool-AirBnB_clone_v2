a6096c69ea2b964b77ec7427fed2340ad56b2ddb
oxyvans <benja.1999.g@hotmail.com>
2022-09-05 12:01:28 +0000

    add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""task 3"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """say hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """c is fun"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """python"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
