14c0492b9808b54f68d50e410892ad1d58a38408
oxyvans <benja.1999.g@hotmail.com>
2022-09-05 17:07:55 +0000

    add task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" TASK 8"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teadown(self):
+    """teardown"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def stateslist():
+    """displays"""
+    List_s = storage.all(State).values()
+    List_s = sorted(List_s, key=lambda k: k.name)
+    return render_template("7-states_list.html", List_s=List_s)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
