5dd4510f027ae20cfc07c911c99c68f2cad11fec
oxyvans <benja.1999.g@hotmail.com>
2022-09-05 17:36:18 -0700

    add task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,6 +13,7 @@ def tdow(self):
     """teardown"""
     storage.close()
 
+
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
@@ -21,6 +22,7 @@ def states(state_id=None):
     if state_id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
