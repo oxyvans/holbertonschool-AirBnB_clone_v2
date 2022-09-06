735e89509859fe9ffcf0027128f2bd0e2105ab2c
oxyvans <benja.1999.g@hotmail.com>
2022-09-05 17:27:45 -0700

    add task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" TASK 9"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tdow(self):
+    """teardown"""
+    storage.close()
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
