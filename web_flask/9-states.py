9cba5b4e085e91bf44f86672c091b9a96e609615
oxyvans <benja.1999.g@hotmail.com>
2022-09-05 17:44:44 -0700

    add task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-""" TASK 9"""
+""" task 10 """
+
 from flask import Flask, render_template
-from models import storage
 from models import *
+from models import storage
 
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def tdow(self):
-    """teardown"""
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
+    """ render """
     states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
 
 
+@app.teardown_appcontext
+def tedow(exception):
+    """ teardown """
+    storage.close()
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port='5000')
